8894ec06cc6dd5c81c54e3c1cdf4bd79a0b7cc4f
Pinki Kumari <160412282+pinkimandal44066@users.noreply.github.com>
2024-10-06 22:22:55 +0530

    homeabout

---
+++ src/Components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Homeone from './Homeone'
+import Footer from './Footer'
+const Home = () => {
+  return (
+    <div>
+      {/* <Navbar />
+      <Homeone />
+      <Footer /> */}
+      <Homeone />
+    </div>
+  )
+}
+
+export default Home
